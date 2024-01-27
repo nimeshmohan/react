7e70b46aee3cb5d1a7cd711b5426c030949a64b5
nimeshmohan <nimeshvellinezhi@gmail.com>
2024-01-28 00:39:38 +0530

    useState consept

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+
+  const addValue = ()=>{
+    if(counter == 20)return
+    counter = counter +1
+    setCounter(counter)
+  }
+  const removeValue =()=>{
+    if (counter == 0) return
+    counter = counter -1
+    setCounter(counter)
+  }
+  return (
+    <>
+      <h1>React Counter</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}>Add value</button>
+      <br /><br />
+      <button onClick={removeValue}>Remove value</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
